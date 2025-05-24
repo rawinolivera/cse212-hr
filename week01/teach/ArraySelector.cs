5cadb92d290706108c6722b4136092be260b3203
rawinolivera <oli18001@byui.edu>
2025-05-23 21:39:08 -0300

    W01 Individual Activity: Dynamic Arrays and Performance Analysis

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,30 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        List<int> intResult = new List<int>();
+        int i1 = 0;
+        int i2 = 0;
+        foreach (int i in select)
+        {
+            if (i == 1)
+            {
+                intResult.Add(list1[i1]);
+                i1++;
+            }
+            else
+            {
+                intResult.Add(list2[i2]);
+                i2++;
+            }
+        }
+        return intResult.ToArray();
     }
 }
