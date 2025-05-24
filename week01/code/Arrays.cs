f868a46d44eb1de3c0c20a7c199e638b1dec5aea
rawinolivera <oli18001@byui.edu>
2025-05-24 02:18:36 -0300

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+
 public static class Arrays
 {
     /// <summary>
@@ -9,11 +11,23 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
+        // First I will define a new array of multiples
+        // then I will use a for to iterate from 1 until it get the length which I use as the limit 
+        // after that will multiply the number by the number of iterations to finally added to the array
+
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        double[] multiples = new double[length];
+        int j = 0;
+        for (double i = 1; i <= length; i++)
+        {
+            multiples[j] = number * i;
+            j++;
+        }
+
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -26,8 +40,19 @@ public static class Arrays
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
+
+        // I will define un for defining the number of iterations according to amount
+        // then I will remove the last item to added to the fisrt position of the list
+
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        for (int i = 1; i <= amount; i++)
+        {
+            int value = data[data.Count - 1];
+            data.RemoveAt(data.Count - 1);
+            data.Insert(0, value);
+        }
     }
 }
